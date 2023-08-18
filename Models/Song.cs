bc3e15738813dba23ac554c37f7615b7ea16f25a
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-08-18 13:00:53 -0500

    First initialization of API, Inital setup completed, ApplicationDbContext Class created, ApplicationDbContext configured as service, model class created.

---
+++ Models/Song.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music_LibraryBackend.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id {get; set;}
+        public string Title {get; set;}
+        public string Artist {get; set;}
+        public string Album {get; set;}
+        public string Genre {get; set;}
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
