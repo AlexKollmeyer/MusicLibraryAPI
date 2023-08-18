f43db3459347dca4b5a9c85555732241faa4e357
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-08-18 13:35:10 -0500

    Public DbSet added, migration completed.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Music_LibraryBackend.Models;
 
 namespace Music_LibraryBackend.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Song> Songs { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
